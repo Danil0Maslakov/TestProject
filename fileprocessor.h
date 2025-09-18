a185c7a75e8dc7dee42131cc84a1323369687d45
Danil0 <danil0maslakov@gmail.com>
2025-09-18 05:49:42 +0300

    first comm

---
+++ fileprocessor.h
@@ -0,0 +1,28 @@
+#ifndef FILEPROCESSOR_H
+#define FILEPROCESSOR_H
+
+#include <QObject>
+#include <QRunnable>
+#include <QFile>
+
+        class FileProcessor : public QObject, public QRunnable
+{
+    Q_OBJECT
+public:
+    // collisionMode: 0 = overwrite, 1 = add counter
+    FileProcessor(const QString &inputPath, const QString &outputDir, bool deleteInput, int collisionMode, const QByteArray &xor8);
+    void run() override;
+
+signals:
+    void progress(qint64 processed, qint64 total, const QString &fileName);
+    void finished(const QString &fileName, bool ok, const QString &message);
+
+private:
+    QString m_inputPath;
+    QString m_outputDir;
+    bool m_deleteInput;
+    int m_collisionMode;
+    QByteArray m_xor8;
+
+    QString chooseOutputPath(const QString &inFile);
+};
